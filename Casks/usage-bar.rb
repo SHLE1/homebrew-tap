77f5183c657d7efbe677d18a8244b76ce5a4d555
SHLE1 <SHLE1@proton.me>
2026-04-13 15:03:49 +0800

    chore: update UsageBar cask to v0.0.6

--- Casks/usage-bar.rb
+++ Casks/usage-bar.rb
@@ -1,8 +1,8 @@
 cask "usage-bar" do
-  version "0.0.1"
-  sha256 "REPLACE_WITH_REAL_SHA256"
+  version "0.0.6"
+  sha256 "e63ad3a23b9e098ba70a01765880857b041bd385c2073a260b41ca6b7b8ce81c"
 
-  url "https://github.com/SHLE1/usage-bar/releases/download/v#{version}/UsageBar-v#{version}.dmg"
+  url "https://github.com/SHLE1/usage-bar/releases/download/v#{version}/UsageBar-v#{version}-unsigned.dmg"
   name "UsageBar"
   desc "Menu bar app for AI provider usage monitoring"
   homepage "https://github.com/SHLE1/usage-bar"
